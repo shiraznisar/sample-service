12715bef190c3ad90243c2f4c0fa1f3052a6f056
Shiraz Nisar <shiraznisar@gmail.com>
2017-12-19 11:20:40 +1100

    initial

---
+++ src/main/java/io/swagger/samples/inflector/springboot/models/UserDetails.java
@@ -0,0 +1,29 @@
+package io.swagger.samples.inflector.springboot.models;
+
+
+public class UserDetails {
+
+	private String surname;
+	private String firstname;
+	private String dob;
+	
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	
+}
